b608cce52f0dfb77c34ee499a8a83eda1bf7c450
Maxim Bureev <mvbureev@gmail.com>
2021-05-16 23:17:33 +0400

    fix: ci

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          {`<${mvbureev}>`}
+          mvbureev
           <br />
           <br />
           vps + domain + docker + ci/cd
@@ -20,8 +20,8 @@ export default function Home() {
         </h1>
 
         <p className={styles.title}>
-          <a className={styles.code} href="https://gitlab.com/mvbureev/next-starter">
-            https://gitlab.com/mvbureev/next-starter
+          <a className={styles.code} href="https://gitlab.com/mvbureev/vps-cicd-starter">
+            gitlab.com/mvbureev/vps-cicd-starter
           </a>
         </p>
         </main>
