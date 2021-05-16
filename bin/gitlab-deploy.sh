479694ece14a2d3873713e483691b26f29017e65
Maxim Bureev <mvbureev@gmail.com>
2021-05-16 11:53:15 +0400

    fix: ci

--- bin/gitlab-deploy.sh
+++ bin/gitlab-deploy.sh
@@ -26,7 +26,7 @@ chmod 400 $DOCKER_CERT_PATH/key.pem
 export DOCKER_TLS_VERIFY=1
 export DOCKER_HOST=tcp://$DEPLOY_HOST:2376
 
-systemctl status docker
+dockerd
 
 # проверим, что коннектится все успешно
 docker-compose \
