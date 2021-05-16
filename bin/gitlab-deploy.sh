ba880a4af34eb226531c5df6fa617d21d77e80cf
Maxim Bureev <mvbureev@gmail.com>
2021-05-16 12:23:27 +0400

    fix: ci

--- bin/gitlab-deploy.sh
+++ bin/gitlab-deploy.sh
@@ -4,27 +4,8 @@ set -e
 # Выводим, то , что делаем
 set -v
 
-# 
 DOCKER_COMPOSE_FILE=docker-compose.yml
-# Куда деплоим (добавлин напрямую в gitlab)
-# DEPLOY_HOST=78.140.136.90
-# Путь для сертификатов клиента, то есть в нашем случае - gitlab-воркера
-DOCKER_CERT_PATH=/root/.docker
-
-# создаем путь (сейчас работаем в клиенте - воркере gitlab'а)
-mkdir $DOCKER_CERT_PATH
-# изымаем содержимое переменных, при этом удаляем лишние символы добавленные при сохранении переменных.
-echo "$CA_PEM" | tr -d '\r' > $DOCKER_CERT_PATH/ca.pem
-echo "$CERT_PEM" | tr -d '\r' > $DOCKER_CERT_PATH/cert.pem
-echo "$KEY_PEM" | tr -d '\r' > $DOCKER_CERT_PATH/key.pem
-# на всякий случай даем только читать
-chmod 400 $DOCKER_CERT_PATH/ca.pem
-chmod 400 $DOCKER_CERT_PATH/cert.pem
-chmod 400 $DOCKER_CERT_PATH/key.pem
-
-# далее начинаем уже работать с удаленным docker-демоном. Собственно, сам деплой
-export DOCKER_TLS_VERIFY=1
-export DOCKER_HOST=tcp://$DEPLOY_HOST:2376
+# export DOCKER_HOST=tcp://$DEPLOY_HOST:2376
 
 # проверим, что коннектится все успешно
 docker-compose \
